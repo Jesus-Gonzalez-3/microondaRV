b6d5d55969a3a0f98ccab9de701a8d17883e8380
Jesus González <jesususgonzalez3@gmail.com>
2022-01-25 13:05:18 -0600

    avance semana 4

---
+++ vendor/dompdf/dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,122 @@
+<?php return function ($fontDir, $rootDir) {
+return array (
+  'sans-serif' => array(
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
+  ),
+  'times' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'times-roman' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'courier' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'helvetica' => array(
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => array(
+    'normal' => $fontDir . '/ZapfDingbats',
+    'bold' => $fontDir . '/ZapfDingbats',
+    'italic' => $fontDir . '/ZapfDingbats',
+    'bold_italic' => $fontDir . '/ZapfDingbats',
+  ),
+  'symbol' => array(
+    'normal' => $fontDir . '/Symbol',
+    'bold' => $fontDir . '/Symbol',
+    'italic' => $fontDir . '/Symbol',
+    'bold_italic' => $fontDir . '/Symbol',
+  ),
+  'serif' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'monospace' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'fixed' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'dejavu sans' => array(
+    'bold' => $fontDir . '/DejaVuSans-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSans-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSans-Oblique',
+    'normal' => $fontDir . '/DejaVuSans',
+  ),
+  'dejavu sans mono' => array(
+    'bold' => $fontDir . '/DejaVuSansMono-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSansMono-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSansMono-Oblique',
+    'normal' => $fontDir . '/DejaVuSansMono',
+  ),
+  'dejavu serif' => array(
+    'bold' => $fontDir . '/DejaVuSerif-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSerif-BoldItalic',
+    'italic' => $fontDir . '/DejaVuSerif-Italic',
+    'normal' => $fontDir . '/DejaVuSerif',
+  ),
+  'font awesome 5 brands' => array(
+    'normal' => $fontDir . '/font_awesome_5_brands_normal_4476c35e32052d6a3bbb9a816917508f',
+  ),
+  'font awesome 5 free' => array(
+    'normal' => $fontDir . '/font_awesome_5_free_normal_6b07648c9201a88d0e201ac56b03d56b',
+    '900' => $fontDir . '/font_awesome_5_free_900_a18f627f6330cb52a36c6ed1eeb9f6fe',
+  ),
+  'simple-line-icons' => array(
+    'normal' => $fontDir . '/simple_line_icons_normal_7488accb52382cfca0566e9d747101f4',
+  ),
+  'weathericons' => array(
+    'normal' => $fontDir . '/weathericons_normal_0eb7fa5210b34b9a002c90b32a68a186',
+  ),
+  'linea-arrows-10' => array(
+    'normal' => $fontDir . '/linea_arrows_10_normal_51ea966120a754ba3d6e36c909ee9472',
+  ),
+  'linea-basic-10' => array(
+    'normal' => $fontDir . '/linea_basic_10_normal_03a2ee95d9927f477d54cea3c6ad3904',
+  ),
+  'linea-basic-elaboration-10' => array(
+    'normal' => $fontDir . '/linea_basic_elaboration_10_normal_84e670614a8bcdb0ac3f28c7c4c8c839',
+  ),
+  'linea-ecommerce-10' => array(
+    'normal' => $fontDir . '/linea_ecommerce_10_normal_e1655a591a6e937fa93b1191ff8630e7',
+  ),
+  'linea-music-10' => array(
+    'normal' => $fontDir . '/linea_music_10_normal_3fa91cbec71eae70960165b714ee81a5',
+  ),
+  'linea-software-10' => array(
+    'normal' => $fontDir . '/linea_software_10_normal_666ec6555ec91bd6bbd1b7c035329e94',
+  ),
+  'linea-weather-10' => array(
+    'normal' => $fontDir . '/linea_weather_10_normal_752c5d59065568aa164fca71367c0abd',
+  ),
+  'themify' => array(
+    'normal' => $fontDir . '/themify_normal_28ac6a34bc131684291d5d6c702552f2',
+  ),
+  'material design icons' => array(
+    'normal' => $fontDir . '/material_design_icons_normal_634119a60ec84b67c289bbce375a918b',
+  ),
+);
+}; ?>
